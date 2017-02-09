8729e2a6685641b9cd86d6e9d3e1bf5777a86b53
Darius Marte <Darius.marte66gmail.com>
2017-02-09 07:06:42 -0500

    Newbran

--- app.js
+++ app.js
@@ -1,45 +1,81 @@
 'use strict';
 
-alert ('Hello');
-var userInput= prompt('What\'s your name');
-var from= prompt('Where are you from ?');
-var liveSit= prompt('what do you like to do ?');
-alert('Interesting....');
-alert("My name is musa");
-alert("I was designed by Darius Marte");
-var knowMe= prompt("do you know him ?");
-
-if (knowMe.toUpperCase() === 'YES' || knowMe.toUpperCase() === 'Y' ) {
-  var birthQ= prompt('I see... well do you know his birthday ?');  
+alert('Hello');
+var userInput = prompt('What\'s your name');
+alert('Hi, ' + userInput);
+alert('My name is musa');
+alert('I was designed by Darius Marte');
+var knowMe = prompt('do you know him ?');
+console.log(userInput);
+console.log(birthQ);
+var birthQ;
+if (knowMe.toUpperCase() === 'YES' || knowMe.toUpperCase() === 'Y') {
+  birthQ = prompt('I see... well do you know his birthday ?');
 }
 else {
-  alert('Not a valid answer my dear =)' );  
+  alert('Not a valid answer my dear =)');
+  birthQ = prompt('Guess his birtday anyway !');
 }
 
-if (birthQ.toUpperCase() ==='NO'|| birthQ.toUpperCase() === 'N') {
-    alert('I thought not');
+
+if (birthQ.toUpperCase() === 'NO' || birthQ.toUpperCase() === 'N') {
+  alert('I thought not');
 }
-else if(birthQ.toUpperCase() === 'YES' || birthQ.toUpperCase() === 'Y') {
- var birthGuess = prompt('when is it ?', '528 or 628');
- if(birthGuess ==='528'){
-     alert('Correct!');
- }
- else{
-     alert('Not Valid'); }
+else if (birthQ.toUpperCase() === 'YES' || birthQ.toUpperCase() === 'Y') {
+  var birthGuess = prompt('when is it ?', '528 or 628');
+  if (birthGuess === '528') { console.log(birthGuess);
+    alert('Correct!');
+  }
+  else {
+    alert('Not Valid, You must mean No');
+    birthQ = 'NO';
+  }
 }
-else{
-    alert('Not valid');  
-}
+
 alert('since you\'re a friend of Darius, he\'d like me to ask');
-
 var howBout = prompt('Are you OK ?', 'YES or NO');
 if (howBout.toUpperCase() === 'YES' || howBout.toUpperCase() === 'Y') {
-    console.log(howBout.toUpperCase() );
-    alert('I\'m happy to recieve that... I know humans typically say \'hear\' but I don\'t have ears !');
-} 
-else {
-    alert('Everything\'s going to be alright !');
+  console.log(howBout.toUpperCase());
+  alert('I\'m happy to hear that ! ');
 }
+else {
+  alert('Everything\'s going to be alright !');
+}
+
+var NumGues = prompt('Can you guess what number I am thinking of, pick 1 through 6, you have 4 attempts');
+
+for(var p = 1; p < 4; p++){
+  if(NumGues === '1' ) {
+    alert('Good Job !! ');
+    break;
+  } else {
+    NumGues = prompt('Try again', '1, 2, 3, 4, 5, 6');
+  }
+};
+
+var correct = false;
+var counter = 0;
+while(counter < 6 && correct === false) {
+  var canGuess = prompt('Can you guess what month I came to Portland, you have 6 attempts, make sure your answers are lowercase');
+  var months = ['july','may','august','september','november','june' ];
+  for(var i = 0; i < months.length; i++){
+    console.log(months);
+    if(canGuess.toLowerCase() === months[i] ) {
+      correct = true;
+    }
+
+  };
+  counter++;
+  if (correct === true) {
+    alert ('You da best !, and you\'re worth it !');
+
+  } 
+  else {
+    alert('nope');
+  }
+}
+
+
 
 
 
